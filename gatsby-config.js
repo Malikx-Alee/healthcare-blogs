b537e35ec5b4f7dc9cc2a92b5850ff9f742a4134
Malik Ali Haider Awan <ali114_haider@outlook.com>
2020-12-08 16:42:40 +0500

    tracking id updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,7 +103,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-8LQQP9VCK0",
+        trackingId: "G-MTD7WK4X60",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
