b372280f9c4f9ae6ae8294e00ab40a771b806288
Malik Ali Haider Awan <ali114_haider@outlook.com>
2020-12-11 18:07:05 +0500

    read from env

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,7 +104,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "G-MTD7WK4X60", // Google Analytics / GA
+          process.env.GOOGLE_ANALYTICS_MEASUREMENTID, // Google Analytics / GA
           // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
